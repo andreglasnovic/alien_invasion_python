a271283805ada8f1e216786d73bb4758e94f8cc0
andreglasnovic <aglasnovic@icloud.com>
2022-11-10 16:34:45 -0500

    added ship, settings files and a ship image

--- alien_venv/alien_invasion.py
+++ alien_venv/alien_invasion.py
@@ -1,27 +1,42 @@
 import sys
 import pygame
 
+from settings import Settings
+from ship import Ship
+
 class AlienInvasion:
 
     def __init__(self) -> None:
         pygame.init()
-
-        self.screen = pygame.display.set_mode((1200,800))
-
+        self.settings = Settings()
+        self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
+        self.ship = Ship(self)
         pygame.display.set_caption("Alien Invasion")
 
 
-        self.bg_color = (230,230,230)
+        
 
     def run_game(self):
         while True:
-            for event in pygame.event.get():
-                print(event.type)
-                if event.type == pygame.QUIT:
-                    sys.exit()
 
-            self.screen.fill(self.bg_color)
-            pygame.display.flip()
+            self._check_events()
+            self._update_screen()
+
+    def _check_events(self):
+
+        for event in pygame.event.get():
+            #print(event.type)
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.rect.x += 1
+                
+    def _update_screen(self):
+
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        pygame.display.flip()
 
     
 
